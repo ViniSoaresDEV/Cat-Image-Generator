d29e9de292fd9d3f8d7f4a83e80eb7fe9bcd85e8
ViniSoaresDEV <148481719+ViniSoaresDEV@users.noreply.github.com>
2025-12-04 22:02:43 -0300

    Add a dark mode button

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 const catBox = document.querySelector("div#cat-box");
 const btnCat = document.querySelector("button#btn-cat");
 const loading = document.querySelector("div#loading");
+const btnToggled = document.querySelector("button#toggled");
+const tittle = document.querySelector("h1#h1");
 
-btnCat.addEventListener('click', requestCat);
+btnCat.addEventListener("click", requestCat);
 
+btnToggled.addEventListener("click", () => {
+  if (!btnToggled.classList.contains("toggled")) {
+    btnToggled.classList.add("toggled");
+    document.body.style.backgroundColor = "#13141d";
+    h1.style.color = "#fff";
+  } else {
+    btnToggled.classList.remove("toggled");
+    document.body.style.backgroundColor = "#fff";
+    h1.style.color = "#000";
+  }
+});
 
 async function requestCat() {
+  catBox.innerHTML = "";
 
-    catBox.innerHTML = "";
+  try {
+    const url = "https://api.thecatapi.com/v1/images/search";
+    const response = await fetch(url);
+    const json = await response.json();
 
-    try{
-        const url = "https://api.thecatapi.com/v1/images/search";
-        const response = await fetch(url);
-        const json = await response.json();
-
-        showCatImage(json[0].url);
-    }catch(error){
-        showMessage("Não achamos nenhum gato :(");
-        console.error("Erro na API:", error);
-    }
+    showCatImage(json[0].url);
+  } catch (error) {
+    showMessage("Não achamos nenhum gato :(");
+    console.error("Erro na API:", error);
+  }
 }
 
-
-function showCatImage(catImage){
-    
-    const createImg = document.createElement('img');
-    createImg.src = `${catImage}`;
-    catBox.innerHTML = '';
-    catBox.appendChild(createImg);
-
+function showCatImage(catImage) {
+  const createImg = document.createElement("img");
+  createImg.src = `${catImage}`;
+  catBox.innerHTML = "";
+  catBox.appendChild(createImg);
 }
 
-function showMessage(msg){
-    catBox.innerHTML = msg;
+function showMessage(msg) {
+  catBox.innerHTML = msg;
 }
